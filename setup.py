acf88c32a52b07a411a3e80136bbe91679c45a36
Reza (Sam) Aghamohammadi <re.agha.mo@gmail.com>
2024-07-31 12:02:16 +0330

    20240731

--- setup.py
+++ setup.py
@@ -1,29 +1,35 @@
-# from setuptools import setup
-#
-# with open("README.md", "r", encoding="utf-8") as handler:
-#     long_description = handler.read()
-#
-# setup(
-#     name="configloader",
-#     version="1.0.1",
-#     description="Config Parser and Loader",
-#     url="https://git.liateam.net/python/packages/configloader.git",
-#     author="Shahab Ghosni",
-#     author_email="s.ghosni@liateam.ir",
-#     long_description=long_description,
-#     long_description_content_type="text/markdown",
-#     license="BSD 2-clause",
-#     packages=["configloader"],
-#     package_dir={"": "."},
-#     install_requires=[
-#         "pydantic >= 1.10",
-#         "packaging >= 23.2",
-#     ],
-#     classifiers=[
-#         "Programming Language :: Python :: 3.6",
-#         "Programming Language :: Python :: 3.8",
-#         "Programming Language :: Python :: 3.9",
-#         "Programming Language :: Python :: 3.10",
-#         "Programming Language :: Python :: 3.11",
-#     ],
-# )
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as handler:
+    long_description = handler.read()
+
+# TODO: read from requirements.txt for multiple os for install_requires
+
+setup(
+    name="utilsuseragent",
+    version="0.0.1.20240731",
+    packages=["utilsuseragent"],
+    install_requires=[
+        # List your project's dependencies here.
+        # Example: 'requests>=2.20.0',
+    ],
+    # entry_points={
+    #     'console_scripts': [
+    #         # 'command-name=package.module:function',
+    #     ],
+    # },
+    author="Reza 'Sam' Aghamohammadi (Hacknitive)",
+    author_email="hacknitive@gmail.com",
+    description="Create and get real useragent strings",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hacknitive/utilsuseragent",
+    license="MIT",
+    package_dir={"": "."},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
